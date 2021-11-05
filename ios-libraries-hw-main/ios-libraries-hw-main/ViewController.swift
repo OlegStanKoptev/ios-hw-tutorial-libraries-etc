63d9f07fc38066d7f7573cd4c931cfcf95d2a9e6
Oleg Koptev <olegstankoptev@gmail.com>
2021-11-05 15:56:35 +0300

    new project added

---
+++ ios-libraries-hw-main/ios-libraries-hw-main/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ios-libraries-hw-main
+//
+//  Created by Oleg Koptev on 05.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func logFromframework(_ sender: Any) {
+    }
+    @IBAction func logFromSwiftPackage(_ sender: Any) {
+    }
+    @IBAction func logFromPod(_ sender: Any) {
+    }
+    @IBAction func logFromCarthage(_ sender: Any) {
+    }
+    
+}
+
